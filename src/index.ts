11302bb135d278d23abfd982d6fb827bb6ed119e
alxevvv <alxevvv@gmail.com>
2023-07-06 17:41:24 +0600

    refactor: rename functions
    BREAKING CHANGE: - function `add` renamed to `sum`
     - function `mult` renamed to `tim`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-function add(x: number, y = x) {
+function sum(x: number, y = x) {
   return x + y;
 }
 
-function mult(x: number, y = x) {
+function tim(x: number, y = x) {
   return x * y;
 }
 
@@ -10,4 +10,4 @@ function pow(x: number, p = 2) {
   return Math.pow(x, p);
 }
 
-export { add, mult, pow };
+export { pow, sum, tim };
