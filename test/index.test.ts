5b5f51b063221a4ada439c73e9ff2f06bb27cbea
alxevvv <alxevvv@gmail.com>
2023-07-06 17:41:24 +0600

    refactor: rename functions
    BREAKING CHANGE: - function `add` renamed to `sum`
     - function `mult` renamed to `tim`

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,25 +1,25 @@
 import { describe, expect, it } from "vitest";
 
-import { add, mult, pow } from "../src";
+import { pow, sum, tim } from "../src";
 
 describe("index.ts", () => {
   describe("function sum", () => {
     it("should sum given numbers", () => {
-      expect(add(2, 3)).toBe(5);
+      expect(sum(2, 3)).toBe(5);
     });
 
     it("should place the first argument's value instead second if latter is omitted", () => {
-      expect(add(2)).toBe(4);
+      expect(sum(2)).toBe(4);
     });
   });
 
-  describe("function mult", () => {
+  describe("function tim", () => {
     it("should sum given numbers", () => {
-      expect(mult(2, 3)).toBe(6);
+      expect(tim(2, 3)).toBe(6);
     });
 
     it("should place the first argument's value instead second if latter is omitted", () => {
-      expect(mult(3)).toBe(9);
+      expect(tim(3)).toBe(9);
     });
   });
 
