eece41f4c872ae599213cbb40754dc4425381ac5
alxevvv <alxevvv@gmail.com>
2023-07-08 21:39:09 +0600

    feat: add `div` function

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,12 @@ function tim(x: number, y = x) {
   return x * y;
 }
 
+function div(x: number, y = 2) {
+  return x / y;
+}
+
 function pow(x: number, p = 2) {
   return Math.pow(x, p);
 }
 
-export { pow, sum, tim };
+export { div, pow, sum, tim };
